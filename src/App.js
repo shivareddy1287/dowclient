a1802e682e71b057cc0db042fe0a0cf834fe8198
shiva reddy <shivareddy1287@gmail.com>
2023-08-14 18:30:37 +0530

    download

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import shivadp from "./shivadp.jpg";
+import Mypdf from "./mypdf";
 
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./main";
+
+import "./App.css";
+
+function ImageUploader() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+
+        <Route exact path="/mypdf" element={<Mypdf />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default ImageUploader;
